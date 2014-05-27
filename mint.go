3be70c34cdc4608ddb960a021ac211de07704d1b
otiai10 <otiai10@gmail.com>
2014-05-27 10:41:28 +0900

    Fmt

--- mint.go
+++ mint.go
@@ -5,18 +5,18 @@ import "fmt"
 import "os"
 
 type ProxyTestee struct {
-    t *testing.T
-    actual interface{}
+	t      *testing.T
+	actual interface{}
 }
 
 func Expect(t *testing.T, actual interface{}) *ProxyTestee {
-    return &ProxyTestee{t, actual}
+	return &ProxyTestee{t, actual}
 }
 func (p *ProxyTestee) ToBe(expected interface{}) {
-    if p.actual == expected {
-        return
-    }
+	if p.actual == expected {
+		return
+	}
 	fmt.Printf("Expected to be `%+v`, but actual `%+v`\n", expected, p.actual)
-    p.t.Fail()
-    os.Exit(1)
+	p.t.Fail()
+	os.Exit(1)
 }
--- mint_test.go
+++ mint_test.go
@@ -4,8 +4,8 @@ import "testing"
 import "github.com/otiai10/mint"
 
 func TestMint(t *testing.T) {
-    mint.Expect(t, 1).ToBe(1)
+	mint.Expect(t, 1).ToBe(1)
 }
 func TestMint_Fail(t *testing.T) {
-    // mint.Expect(t, 2).ToBe(1)
+	// mint.Expect(t, 2).ToBe(1)
 }
