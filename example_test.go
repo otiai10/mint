d303a3f4f158e3e68ae9b636a23badfd6a25778c
otiai10 <otiai10@gmail.com>
2014-05-31 22:51:35 +0900

    Update Example

--- example_test.go
+++ example_test.go
@@ -5,7 +5,7 @@ import "testing"
 
 // This is example of `Expect` and `ToBe`
 // `ToBe` can assert to eqaal
-func Example_Expect_ToBe(t *testing.T) {
+func ExampleMint_Expect_ToBe(t *testing.T) {
 	yours := 100
 	expected := 100
 	mint.Expect(t, yours).ToBe(expected)
@@ -14,7 +14,7 @@ func Example_Expect_ToBe(t *testing.T) {
 // This is example of `Dry`
 // `Dry` does NOT os.Exit(1)
 // but returns `ProxyTestee` with `Result`
-func Example_Dry(t *testing.T) {
+func ExampleMint_Dry(t *testing.T) {
 	yours := 100
 	expected := 100
 	result := mint.Expect(t, yours).Dry().ToBe(expected).Result
