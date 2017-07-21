81d1346bf815b714e5929dc6e9d1bec8c346003f
Hiromu OCHIAI <otiai10@gmail.com>
2017-07-21 11:36:51 +0200

    Change context message output a bit

--- because.go
+++ because.go
@@ -4,12 +4,12 @@ import "testing"
 
 // Because is context printer.
 func Because(t *testing.T, context string, wrapper func(*testing.T)) {
-	Log("  Because:", context, "\n")
+	Log("  Because ", context, "\n")
 	wrapper(t)
 }
 
 // When is an alternative of `Because`
 func When(t *testing.T, context string, wrapper func(*testing.T)) {
-	Log("  When:", context, "\n")
+	Log("  When ", context, "\n")
 	wrapper(t)
 }
